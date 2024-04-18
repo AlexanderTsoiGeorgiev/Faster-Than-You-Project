e8319bf4ea4b9202707fbddc55f13c505ce7a934
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2024-04-19 01:45:19 +0300

    Added Initial Driver Card Transition

--- Client/src/components/Card/DriverCard.tsx
+++ Client/src/components/Card/DriverCard.tsx
@@ -1,20 +1,27 @@
 import AllDriversDTO from "@/DTOs/Driver/AllDriversDTO";
+// import { Monaco } from "../../assets/CountryFlags/CountryFlags";
 
-export default function DriverCard(  {driver} : { driver: AllDriversDTO }) {
-  return (
-    <div className="flex flex-col bg-secondary-300 rounded-lg px-2 py-1">
-        <div>Driver</div>
-        <div className="flex flex-row justify-between">
-            <div>
-                <span>{driver.fullName}</span>
+// === "MON" ? (
+//   <img src={`${Monaco}`} />
+// ) : (
+//   <div>Nada </div>
+// )
+
+export default function DriverCard({ driver }: { driver: AllDriversDTO }) {
+    return (
+        <div className="flex flex-col bg-secondary-300 rounded-lg px-2 py-1 hover:-translate-y-2 transition-all">
+            <div>Driver</div>
+            <div className="flex flex-row justify-between">
+                <div>
+                    <span>{driver.fullName}</span>
+                    <div>WHAA</div>
+                </div>
+                <span>{driver.countryCode}</span>
             </div>
-            <span>{driver.countryCode}</span>
+            <div className="flex flex-col">
+                <span>{driver.teamName}</span>
+                <div>DRIVER IMAGE</div>
+            </div>
         </div>
-        <div className="flex flex-col">
-            <span>{driver.teamName}</span>
-            <div>DRIVER IMAGE</div>
-        </div>
-        
-    </div>
-  )
+    );
 }
